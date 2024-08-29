ed990f4512632405fc47abca267e9023ad9e982f
hosseinvalikhani <hosseinv.dev@gmail.com>
2024-08-29 22:01:28 +0330

    feat: country flag and name

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,51 @@
 <script setup lang="ts">
-import { ref } from "vue";
-const countriesData = ref(null);
+import { onBeforeMount } from "vue";
+let countryData: any = [];
 
-fetch("https://restcountries.com/v3.1/all")
-  .then((response) => response.json())
-  .then((data) => (countriesData.value = data.data));
+onBeforeMount(() => {
+  fetch("https://restcountries.com/v3.1/all")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      countryData = data.slice(1, 11);
+      console.log(countryData);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
-console.log(countriesData);
-
-const products = ref(null);
-fetch("https://testapi.jasonwatmore.com/products")
-  .then((response) => response.json())
-  .then((data) => (products.value = data));
-
-console.log(products);
+  //   fetch(url, {
+  //   headers: {
+  //     "x-api-key": api_key,
+  //   },
+  // })
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     let imagesData: any = data;
+  //     console.log(imagesData);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+});
 </script>
 
 <template>
-  {{ products }}
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <img src="" alt="" />
+  <div
+    v-for="item in countryData"
+    class="flex flex-col items-center justify-center"
+    :key="item.cca3"
+  >
+    <div class="flex">
+      <img :src="item.flags.svg" alt="" class="w-8 h-8" :key="item.cca3" />
+      <p :key="item.cca3">{{ item.name.official }}</p>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
