fb8b4562c45bc2257a559aa7f53036815b885e9a
hosseinvalikhani <hosseinv.dev@gmail.com>
2024-08-29 13:49:33 +0330

    start of project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const countriesData = ref(null);
+
+fetch("https://restcountries.com/v3.1/all")
+  .then((response) => response.json())
+  .then((data) => (countriesData.value = data.data));
+
+console.log(countriesData);
+
+const products = ref(null);
+fetch("https://testapi.jasonwatmore.com/products")
+  .then((response) => response.json())
+  .then((data) => (products.value = data));
+
+console.log(products);
+</script>
+
+<template>
+  {{ products }}
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
