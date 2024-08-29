38b9d19f598d06149e995c5ffe4656629d7ba3b1
hosseinvalikhani <hosseinv.dev@gmail.com>
2024-08-29 23:34:22 +0330

    feat: responsive for all size

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ onBeforeMount(() => {
       return response.json();
     })
     .then((data) => {
-      countryData = data.slice(1, 11);
+      countryData = data;
       console.log(countryData);
     })
     .catch(function (error) {
@@ -35,15 +35,16 @@ onBeforeMount(() => {
 
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <img src="" alt="" />
   <div
-    v-for="item in countryData"
-    class="flex flex-col items-center justify-center"
-    :key="item.cca3"
+    class="grid grid-cols-1 w-100vh items-center justify-center gap-2 overflow-y-auto sm:grid-cols-2 lg:grid-cols-3 xl:lg:grid-cols-4"
   >
-    <div class="flex">
-      <img :src="item.flags.svg" alt="" class="w-8 h-8" :key="item.cca3" />
-      <p :key="item.cca3">{{ item.name.official }}</p>
+    <div
+      v-for="item in countryData"
+      :key="item.name.official"
+      class="flex gap-x-[20px] items-center bg-blue-100 p-4 w-[250px] h-[80px]"
+    >
+      <img :src="item.flags.svg" alt="" class="w-8 h-8" />
+      <p class="text-left">{{ item.name.official }}</p>
     </div>
   </div>
 </template>
